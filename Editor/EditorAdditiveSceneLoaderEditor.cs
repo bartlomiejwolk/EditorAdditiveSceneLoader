065383950359d3f02aec6ca6cbea45d3b77f662a
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-08-11 10:38:18 +0200

    Initial commit

---
+++ Editor/EditorAdditiveSceneLoaderEditor.cs
@@ -0,0 +1,146 @@
+﻿#define DEBUG_LOGGER
+
+// Copyright (c) 2015 Bartlomiej Wolk (bartlomiejwolk@gmail.com)
+//  
+// This file is part of the EditorAdditiveSceneLoader extension for Unity.
+// Licensed under the MIT license. See LICENSE file in the project root folder.
+
+using System.Collections.Generic;
+using FileLogger;
+using Rotorz.ReorderableList;
+using UniRx;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorAdditiveSceneLoaderModule {
+
+    [CustomEditor(typeof(EditorAdditiveSceneLoader))]
+    [CanEditMultipleObjects]
+    public sealed class EditorAdditiveSceneLoaderEditor : Editor {
+        #region FIELDS
+
+        private EditorAdditiveSceneLoader Script { get; set; }
+
+        #endregion FIELDS
+
+        #region SERIALIZED PROPERTIES
+
+        private SerializedProperty description;
+        private SerializedProperty scenesToLoad;
+
+        #endregion SERIALIZED PROPERTIES
+
+        #region UNITY MESSAGES
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            DrawVersionLabel();
+            DrawDescriptionTextArea();
+
+            EditorGUILayout.Space();
+
+            DrawSceneToLoadList();
+
+            EditorGUILayout.BeginHorizontal();
+
+            DrawLoadScenesButton();
+            DrawUnloadScenesButton();
+
+            EditorGUILayout.EndHorizontal();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+        private void OnEnable() {
+            Script = (EditorAdditiveSceneLoader)target;
+
+            description = serializedObject.FindProperty("description");
+            scenesToLoad = serializedObject.FindProperty("scenesToLoad");
+        }
+
+        #endregion UNITY MESSAGES
+
+        #region INSPECTOR CONTROLS
+        private void DrawUnloadScenesButton() {
+            var buttonPressed = GUILayout.Button(new GUIContent(
+                "Unload Scenes",
+                "Destroy loaded scene game objects. Each must have a tag " +
+                "with the scene name attached."));
+
+            if (buttonPressed) {
+                var scenesNo = scenesToLoad.arraySize;
+
+                for (int i = 0; i < scenesNo; i++) {
+                    var sceneProperty = scenesToLoad.GetArrayElementAtIndex(i);
+                    var sceneObject = sceneProperty.objectReferenceValue;
+                    var sceneName = "";
+                    var sceneObjectString = sceneObject.ToString();
+                    if (sceneObjectString.Contains(" (UnityEngine.SceneAsset)")) {
+                        sceneName = sceneObjectString.Substring(
+                            0,
+                            sceneObjectString.IndexOf(
+                                " (UnityEngine.SceneAsset)"));
+                    }
+
+                    DestroyImmediate(GameObject.FindWithTag(sceneName));
+                }
+            }
+        }
+
+        private void DrawLoadScenesButton() {
+            var buttonPressed = GUILayout.Button(new GUIContent(
+                "Load Scenes",
+                "Load scenes additively in order specified in the " +
+                "list"));
+
+            if (buttonPressed) {
+                LoadScenesAdditively();
+            }
+        }
+        private void DrawSceneToLoadList() {
+            ReorderableListGUI.Title("Scenes");
+            ReorderableListGUI.ListField(scenesToLoad);
+        }
+
+
+        private void DrawVersionLabel() {
+            EditorGUILayout.LabelField(
+                string.Format(
+                    "{0} ({1})",
+                    EditorAdditiveSceneLoader.Version,
+                    EditorAdditiveSceneLoader.Extension));
+        }
+
+        private void DrawDescriptionTextArea() {
+            description.stringValue = EditorGUILayout.TextArea(
+                description.stringValue);
+        }
+
+        #endregion INSPECTOR
+
+        #region METHODS
+        private void LoadScenesAdditively() {
+            // Number of scenes to load.
+            var scenesNo = scenesToLoad.arraySize;
+            for (int i = 0; i < scenesNo; i++) {
+                var sceneProperty = scenesToLoad.GetArrayElementAtIndex(i);
+                var sceneObject = sceneProperty.objectReferenceValue;
+                var scenePath =
+                    AssetDatabase.GetAssetOrScenePath(sceneObject);
+                // Load scene.
+                EditorApplication.OpenSceneAdditive(scenePath);
+            }
+        }
+
+
+        [MenuItem("Component/Modules/EditorAdditiveSceneLoader")]
+        private static void AddEntryToComponentMenu() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(EditorAdditiveSceneLoader));
+            }
+        }
+
+        #endregion METHODS
+    }
+
+}
---
+++ EditorAdditiveSceneLoader.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) 2015 Bartlomiej Wolk (bartlomiejwolk@gmail.com)
+//  
+// This file is part of the EditorAdditiveSceneLoader extension for Unity.
+// Licensed under the MIT license. See LICENSE file in the project root folder.
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EditorAdditiveSceneLoaderModule {
+
+    public sealed class EditorAdditiveSceneLoader : MonoBehaviour {
+
+        #region CONSTANTS
+
+        public const string Version = "v0.1.0";
+        public const string Extension = "EditorAdditiveSceneLoader";
+
+        #endregion CONSTANTS
+
+        #region DELEGATES
+        #endregion DELEGATES
+
+        #region EVENTS
+        #endregion EVENTS
+
+        #region FIELDS
+
+#pragma warning disable 0414
+        /// <summary>
+        ///     Allows identify component in the scene file when reading it with
+        ///     text editor.
+        /// </summary>
+        [SerializeField]
+        private string componentName = "EditorAdditiveSceneLoader";
+#pragma warning restore 0414
+
+        #endregion FIELDS
+
+        #region INSPECTOR FIELDS
+
+        [SerializeField]
+        private string description = "Description";
+
+        /// <summary>
+        /// Scenes to load additively.
+        /// </summary>
+        [SerializeField]
+        private List<Object> scenesToLoad; 
+
+        #endregion INSPECTOR FIELDS
+
+        #region PROPERTIES
+
+        /// <summary>
+        ///     Optional text to describe purpose of this instance of the component.
+        /// </summary>
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        /// <summary>
+        /// Scenes to load additively.
+        /// </summary>
+        public List<Object> ScenesToLoad {
+            get { return scenesToLoad; }
+            set { scenesToLoad = value; }
+        }
+
+        #endregion PROPERTIES
+
+        #region UNITY MESSAGES
+
+        private void Awake() { }
+
+        private void FixedUpdate() { }
+
+        private void LateUpdate() { }
+
+        private void OnEnable() { }
+
+        private void Reset() { }
+
+        private void Start() { }
+
+        private void Update() { }
+
+        private void OnValidate() { }
+
+        private void OnDisable() { }
+
+        private void OnDestroy() { }
+
+        #endregion UNITY MESSAGES
+
+        #region EVENT INVOCATORS
+        #endregion INVOCATORS
+
+        #region EVENT HANDLERS
+        #endregion EVENT HANDLERS
+
+        #region METHODS
+        #endregion METHODS
+
+    }
+
+}
